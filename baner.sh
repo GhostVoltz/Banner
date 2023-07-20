d44ce363d89e8971fa12e62082cf14c66dcd25c1
GhostVoltz <67173603+GhostVoltz@users.noreply.github.com>
2023-07-20 00:45:29 -0300

    baner.sh

--- baner.sh
+++ baner.sh
@@ -5,5 +5,5 @@ pkg install figlet -y
 apt-get install ruby -y
 gem install lolcat
 cp bash.bashrc /data/data/com.termux/files/usr/etc
-figlet @GhostVoltz
+figlet @GhostVoltz_
 termux-open https://www.tiktok.com/@ghostvoltz_?t=8e8TsZum11a&_r=1
