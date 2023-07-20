ac4b61c33e0e785914f81c7010429315f07a9835
GhostVoltz <67173603+GhostVoltz@users.noreply.github.com>
2023-07-20 00:09:07 -0300

    bash.sh

---
+++ bash.sh
@@ -0,0 +1,9 @@
+cd /data/data/com.termux/files/usr/etc
+rm -rf motd
+cd ~/baner
+pkg install figlet -y
+apt-get install ruby -y
+gem install lolcat
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+figlet @GhostVoltz
+termux-open https://www.tiktok.com/@ghostvoltz_?t=8e8TsZum11a&_r=1
